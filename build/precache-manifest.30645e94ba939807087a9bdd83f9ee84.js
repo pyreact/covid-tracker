ac1daeb1afd2879d3a2752cc8bf01fe512c30f0c
sukesh shetty <arrebhaibhaibhai1@gmail.com>
2020-11-18 12:55:41 +0530

    Add files via upload

---
+++ build/precache-manifest.30645e94ba939807087a9bdd83f9ee84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f27c6e89ec6ef0b72f119de9d0b145",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2955dba80e90341e1851",
+    "url": "/static/css/2.ddfbff33.chunk.css"
+  },
+  {
+    "revision": "c0b999b7807d4b53db52",
+    "url": "/static/css/main.7bcd940e.chunk.css"
+  },
+  {
+    "revision": "2955dba80e90341e1851",
+    "url": "/static/js/2.5dbe52e8.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/static/js/2.5dbe52e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0b999b7807d4b53db52",
+    "url": "/static/js/main.56c656d0.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
